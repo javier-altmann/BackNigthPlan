2a6244d27aced251696f9eab7ed2ab75aff86d73
javieraltmann <jaltmann@glamit.com.ar>
2018-05-26 19:05:30 -0300

    Agrego propiedad al DTO que me olvide de subir en el commit anterior

--- NigthPlan/NightPlan/Core/DTO/UsuarioDelGrupoDTO.cs
+++ NigthPlan/NightPlan/Core/DTO/UsuarioDelGrupoDTO.cs
@@ -6,7 +6,8 @@ namespace Core.DTO
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string ImagenPerfil { get; set; }
-        public bool RespondioSusPreferencias { get; set; }
+        public bool Resultado { get; set; }
+
 
     }
 }
